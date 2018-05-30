7280d78b7a0253587065a31834ddb248ae976448
Chris <chris@cookseys.org>
2018-05-30 11:49:56 -0400

    Changed location of index

---
+++ src/main/java/org/cookseys/partytime/PartyDetails.java
@@ -0,0 +1,15 @@
+package org.cookseys.partytime;
+
+public class PartyDetails {
+
+	private Integer guests;
+	
+	public PartyDetails(int guests) {
+		this.guests = guests;
+	}
+
+	public int getGuests() {
+		return guests;
+	}
+
+}
